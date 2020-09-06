1e85a147ed525055c7ad7ab2a32e7eb8720961ab
30072007 <70792690+30072007@users.noreply.github.com>
2020-09-06 10:33:34 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+#initailazation
+pygame.init()
+
+#display here, >>((800, 600))<< you can tell your height and length in pixels
+screen = pygame.display.set_mode((800, 600))
+
+#Text and icon
+pygame.display.set_caption("name of your window")
+icon = pygame.image.load('example.png')
+pygame.display.set_icon(icon)
